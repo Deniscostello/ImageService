55cdad2b96ac27dddf27b89449ac3305dec1545b
Denis Costello <G00396309@atu.ie>
2024-01-09 14:12:06 +0000

    Added save image method to controller

--- src/main/java/ie/atu/cicdprojectimageservice/ImageController.java
+++ src/main/java/ie/atu/cicdprojectimageservice/ImageController.java
@@ -1,7 +1,17 @@
 package ie.atu.cicdprojectimageservice;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ImageController {
+
+    @PostMapping("addImage")
+    public ResponseEntity<Map<String, List<Image>>> saveImage(){
+
+    }
 }
