2393a6683319cab71451cf49ad079fce7cf8e33c
Denis Costello <G00396309@atu.ie>
2024-01-09 14:08:29 +0000

    added tite and url to image class

--- src/main/java/ie/atu/cicdprojectimageservice/Image.java
+++ src/main/java/ie/atu/cicdprojectimageservice/Image.java
@@ -1,4 +1,13 @@
 package ie.atu.cicdprojectimageservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Image {
+    private String title;
+    private String url;
 }
