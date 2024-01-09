9abfc5889d14fe07e7c1b60a5f484756a0773814
Denis Costello <G00396309@atu.ie>
2024-01-09 13:21:01 +0000

    First commit

---
+++ src/test/java/ie/atu/cicdprojectimageservice/CiCdProjectImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdprojectimageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdProjectImageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
